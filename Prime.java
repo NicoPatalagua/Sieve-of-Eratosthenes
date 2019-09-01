787c68e2ea636ef8d435f5ca3eafaace2140c08f
Nico Patalagua <nicopatalagua@gmail.com>
2019-09-01 15:48:23 -0500

    Code

---
+++ Prime.java
@@ -0,0 +1,18 @@
+#Author: Nico Patalagua
+public class Prime {
+    public static void main(String[] args) {
+        int n = 100000000;
+        boolean isPrime;
+        for (int i= 2; i<=n; i++) {
+            isPrime = true;
+            for (int j = 2; j <= Math.sqrt(i) && isPrime; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                }
+            }
+            if (isPrime) {
+                System.out.println(i);
+            }
+        }
+    }
+}
